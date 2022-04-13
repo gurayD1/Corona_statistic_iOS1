d45a4492d84a0ef05bf0300e4ee0476bcad610b8
Guray <guray1955@gmail.com>
2022-04-12 20:21:00 -0400

    Initial Commit

---
+++ Final_Project_Guray/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final_Project_Guray
+//
+//  Created by Guray Demirezen on 2022-04-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
